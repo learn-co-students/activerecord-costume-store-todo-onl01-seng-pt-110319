de1e305812b30d5ed5554ce54938f63f72af949b
github username <github email address>
2020-02-04 12:24:14 -0800

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,7 +11,6 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
         t.datetime :opening_date
         t.datetime :closing_date
         t.text :description
-        
         t.timestamps
       end
     end
