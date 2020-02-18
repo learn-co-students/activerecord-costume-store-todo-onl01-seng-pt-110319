dc076a10c54d7c9489323fa12a9a657b82ee311e
rhaynes3k <rhaynes3k@gmail.com>
2020-02-17 23:32:38 -0600

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,7 +1,5 @@
 class Costume < ActiveRecord::Base
-  def initialize
-    
-  end
+
 end
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,7 +1,12 @@
-class Costume < ActiveRecord::Base
+class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
     t.string :name
+    t.float :price
+    t.string :image_url
+    t.string :size
+    t.datetime :created_at
+    t.datetime :updated_at
     end
   end
 end
